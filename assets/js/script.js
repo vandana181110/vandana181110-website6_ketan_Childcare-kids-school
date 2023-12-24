e78de76bf549df3ffd6835485eeaab00e54ac75d
vandana181110 <vandanadake29@mail.com>
2023-12-24 08:15:59 -0500

    comlete projet

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// index page testimonial section
+var sliderMain = document.querySelector('.slider-main');
+let item = sliderMain.getElementsByClassName('item');
+
+function next(){
+   sliderMain.append(item[0]);
+}
+function prev(){
+   sliderMain.prepend(item[item.length-1]); 
+}
+// setInterval(
+//    next,3000);
+
+
+function gohome()
+{
+window.location="../index.html";
+}
+
+// about page partner setion
